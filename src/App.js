31776087e7bce1ef17f369ce7811414df5520aee
James Dindi <dindijjames@gmail.com>
2018-04-28 20:08:55 +0300

    Weather component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Titles from './components/Titles';
+import Forms from './components/Forms';
 
 class App extends Component{
   render(){
     return(
       <div>
         <Titles />
+        <Forms />
       </div>
     );
   }
---
+++ src/components/Forms.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Forms extends Component {
+    render(){
+        return(
+            <div>
+                Form Component
+            </div>
+        );
+    }
+};
+
+export default Forms;
